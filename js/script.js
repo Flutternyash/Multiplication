54ec4ba5ae25015764b5a9d922dc216679b0d971
Flutt <fuckingCode@protonmail.com>
2022-04-04 02:16:40 +0300

    script

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// let oneRow = "";
+// function multiply() {
+//   for (let i = 1; i <= 9; i++) {
+//     for (let j = 1; j <= 9; j++) {
+//       oneRow  += (j*i) + " ";
+//     }
+//     document.write(oneRow);
+//     oneRow = "";
+//   }
+// }
+// multiply();
+
+// document.write('</tr>');
+// for (let i = 1; i <= 9; i++) {
+//   for (let j = 1; j <= 9; j++) {
+//     document.write('<td>' + (i*j) + '</td>');
+//   }
+//   document.write('</tr>');
+// }
+
+let table, cell, row, i, j;
+table = document.createElement('table');
+table.style.textAlign = 'center';
+for (let i = 1; i <= 9; i++) {
+  row = document.createElement('tr');
+  for (let j = 1; j <= 9; j++) {
+    cell = document.createElement(i === 1 || j === 1 ? 'th' : 'td');
+    cell.appendChild(document.createTextNode('' + i*j));
+    cell.style.padding = '25px';
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
+}
+document.body.appendChild(table);
